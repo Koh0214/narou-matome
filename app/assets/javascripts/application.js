d66754151affced1f829a1ff59960e8164d25aba
Kohei0214 <kkishimotok@gmail.com>
2018-06-27 00:05:34 +0900

    add readmore to novel-card description
    もっと見るを追加。
    
    matomes.jsから読み込もうと思ったらうまく読めず、
    とりあえずapplication.jsに直書き。

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,27 @@
 //= require activestorage
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+    var count = 100;
+ $('.text_overflow').each(function() {
+     var thisText = $(this).text();
+      var textLength = thisText.length;
+       if (textLength > count) {
+            var showText = thisText.substring(0, count);
+            var hideText = thisText.substring(count, textLength);
+           var insertText = showText;
+          insertText += '<span class="hide">' + hideText + '</span>';
+           insertText += '<span class="omit">…</span>';
+            insertText += '<a href="" class="more">もっと見る</a>';
+            $(this).html(insertText);
+       };
+  });
+ $('.text_overflow .hide').hide();
+ $('.text_overflow .more').click(function() {
+      $(this).hide()
+          .prev('.omit').hide()
+         .prev('.hide').fadeIn();
+      return false;
+   });
+});
