023f7430764c20d50ebe92c2140c04cb00e5561f
Kohei0214 <kkishimotok@gmail.com>
2018-07-03 01:00:57 +0900

    matomes _novel-card view | fix layout, fix color of もっと見る

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -32,7 +32,7 @@ $(document).on('turbolinks:load',function(){
            var insertText = showText;
           insertText += '<span class="hide">' + hideText + '</span>';
            insertText += '<span class="omit">…</span>';
-            insertText += '<a href="" class="more">もっと見る</a>';
+            insertText += '<a href="" class="more" style="color: #b2b2b2">もっと見る</a>';
             $(this).html(insertText);
        };
   });
